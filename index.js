9946a5750330850b3d4ec3ba72d828e19b41490a
Evans Ochieng <evansochieng121@gmail.com>
2022-08-15 16:31:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+// customerName variable
+var customerName = 'bob';
+
+// upperCaseCustomerName() function
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+};
+
+// setBestCustomer() function
+var bestCustomer;
+function setBestCustomer() {
+    //var bestCustomer = 'not bob';
+    return bestCustomer = 'not bob';
+};
+
+// overwriteBestCustomer() function
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+};
+
+// changeLeastFavoriteCustomer() function
+const leastFavoriteCustomer = 'Evans';
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'Ochieng';
+};
